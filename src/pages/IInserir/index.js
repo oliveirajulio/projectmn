370aae419298129b8079a60934ff47b453e36283
oliveirajulio <juliocesargomes33@gmail.com>
2024-11-11 15:46:09 -0300

    center

--- src/pages/IInserir/index.js
+++ src/pages/IInserir/index.js
@@ -33,10 +33,14 @@ function Inserir_Nota () {
                     <div className="layout-initial">
                         <h1 className="main">Entrada de nota</h1>
                         <h2 className="inf2">Insira todos os dados necessários, como CNPJ, data e itens.  A precisão é crucial para evitar erros e garantir conformidade fiscal.</h2>
-                        <button className="btn" onClick={fileselect}>Selecionar NF</button>
+                        <div className="btn-group">
+                            <button className="btn" onClick={fileselect}>Selecionar NF</button>
+                        <div className="sd-container">
+                            <button className="sd"></button>
+                            <button className="sd1"></button>
+                        </div>
+                        </div>
                         <h3 className="uploader">ou arraste e solte seu arquivo aqui</h3>
-                        <button className="sd"></button>
-                        <button className="sd1"></button>
                     </div>    
                 </>
                 ) : (
